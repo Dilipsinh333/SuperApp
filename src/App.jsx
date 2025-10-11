7e05a79b6d69a2646ce7106853e49800ae2bbfda
Dilipsinh Sindhav <dilipsindhav33@gmail.com>
2025-10-11 10:48:10 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Movies from "./pages/Movies";
+import Browse from "./pages/Browse";
+import Homepage from "./pages/Homepage";
+import { useContext } from "react";
+import { AuthContext } from "./context/AuthContext";
+function App() {
+  const {user}= useContext(AuthContext);
+  return (
+      <Routes>
+        <Route path="/register" element={<Register />}></Route>
+        <Route path="/browse" element={user?(user.preference?<Browse />:<Movies/>):<Register/>}></Route>
+        <Route path="/" element={user?(user.preference?<Homepage />:<Movies/>):<Register/>}></Route>
+        <Route path="/movie" element={user?<Movies />:<Register />}></Route>
+      </Routes>
+  );
+}
+
+export default App;
